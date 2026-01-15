3e31013cf061d4e5200c9a13ed3f4b7584d6e046
Anton Arizal <antonarizal@gmail.com>
2026-01-15 15:17:42 +0700

    change sidebar to sidebar collapsible

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -1,5 +1,5 @@
-<x-layouts.admin.sidebar :title="$title ?? null">
+<x-layouts.admin.sidebar-collapsible :title="$title ?? null">
     <flux:main>
         {{ $slot }}
     </flux:main>
-</x-layouts.admin.sidebar>
+</x-layouts.admin.sidebar-collapsible>
