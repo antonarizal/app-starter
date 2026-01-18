a815e2f096f52614a973f7c14223ce138b3b5706
Anton Arizal <antonarizal@gmail.com>
2026-01-18 15:19:42 +0700

    role id

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('admin');
+            $table->integer('role_id')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
