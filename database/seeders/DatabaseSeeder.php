ea8c4e9362ab5501f46897bd57841a8b60ae443c
Anton Arizal <antonarizal@gmail.com>
2026-01-18 15:20:37 +0700

    add data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Role;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Option;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,9 +21,36 @@ class DatabaseSeeder extends Seeder
             ['email' => 'admin@gmail.com'],
             [
                 'name' => 'Admin',
-                'password' => bcrypt('password'),
+                'password' => bcrypt('admin123'),
                 'email_verified_at' => now(),
+                'role' => 'admin',
+                'role_id' => 1,
             ]
         );
+        Role::firstOrCreate(
+            ['id' => 1],
+            [
+                'role_name' => 'admin',
+                'role_description' => 'Administrator role with full permissions',
+                'permissions' => json_encode(['create', 'read', 'update', 'delete']),
+            ]
+        );
+        Role::firstOrCreate(
+            ['id' => 2],
+            [
+                'role_name' => 'user',
+                'role_description' => 'Standard user role with limited permissions',
+                'permissions' => json_encode(['read']),
+            ]
+        );
+        Option::firstOrCreate(
+            ['option_name' => 'site_name'],
+            ['option_value' => 'My Application']
+        );
+        Option::firstOrCreate(
+            ['option_name' => 'site_description'],
+            ['option_value' => 'This is my application description.']
+        );
+
     }
 }
